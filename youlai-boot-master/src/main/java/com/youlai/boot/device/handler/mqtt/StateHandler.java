8e66956ee4857404c214ed326ff35cae0ec00bf9
way <1556080769@qq.com>
2025-07-04 17:51:13 +0800

    mqtt传感器influxdb存储

--- youlai-boot-master/src/main/java/com/youlai/boot/device/handler/mqtt/StateHandler.java
+++ youlai-boot-master/src/main/java/com/youlai/boot/device/handler/mqtt/StateHandler.java
@@ -3,9 +3,14 @@ package com.youlai.boot.device.handler.mqtt;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.domain.WritePrecision;
 import com.youlai.boot.common.constant.RedisConstants;
+import com.youlai.boot.config.property.InfluxDBProperties;
 import com.youlai.boot.device.handler.service.MsgHandler;
 import com.youlai.boot.device.model.entity.Device;
+import com.youlai.boot.device.model.influx.InfluxSensor;
+import com.youlai.boot.device.model.vo.DeviceInfo;
 import com.youlai.boot.device.service.DeviceService;
 import com.youlai.boot.device.topic.HandlerType;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +37,8 @@ import static com.youlai.boot.common.util.MacUtils.getCodeByTopic;
 public class StateHandler implements MsgHandler {
     private final RedisTemplate<String, Object> redisTemplate;
     private final DeviceService deviceService;
+    private final InfluxDBClient influxDBClient;
+    private final InfluxDBProperties influxProperties;
 
     @Override
     public void process(String topic, String jsonMsg, MqttClient mqttClient) {
@@ -122,11 +129,34 @@ public class StateHandler implements MsgHandler {
                 if (ObjectUtils.isNotEmpty(deviceCache)) {
                     device.setStatus(1);
                     redisTemplate.opsForHash().put(RedisConstants.Device.DEVICE, deviceCode, device);
-//                    deviceService.updateById(device);
                 } else {
                     device.setStatus(1);
-//                    deviceService.updateById(device);
                 }
+                JsonNode deviceInfo = deviceCache.getDeviceInfo();
+                //创建influx数据
+                InfluxSensor point = new InfluxSensor();
+                //tag为设备编号
+                point.setDeviceCode(deviceCache.getDeviceCode());
+                //tag为房间编号
+                point.setRoomId(deviceCache.getDeviceRoom().toString());
+                if (deviceInfo.has("DHT11")) {
+                    JsonNode data = deviceInfo.get("DHT11");
+                    //温度
+                    if (data.has("Temperature")) {
+                        point.setTemperature(data.get("Temperature").asDouble());
+
+                    }
+                    //湿度
+                    if (data.has("Humidity")) {
+                        point.setHumidity(data.get("Humidity").asDouble());
+                    }
+                }
+                influxDBClient.getWriteApiBlocking().writeMeasurement(
+                        influxProperties.getBucket(),
+                        influxProperties.getOrg(),
+                        WritePrecision.MS,
+                        point
+                );
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -151,11 +181,53 @@ public class StateHandler implements MsgHandler {
                 if (ObjectUtils.isNotEmpty(deviceCache)) {
                     device.setStatus(1);
                     redisTemplate.opsForHash().put(RedisConstants.Device.DEVICE, deviceCode, device);
-//                    deviceService.updateById(device);
                 } else {
                     device.setStatus(1);
-//                    deviceService.updateById(device);
                 }
+                JsonNode deviceInfo = deviceCache.getDeviceInfo();
+                //创建influx数据
+                InfluxSensor point = new InfluxSensor();
+                //tag为设备编号
+                point.setDeviceCode(deviceCache.getDeviceCode());
+                //tag为房间编号
+                point.setRoomId(deviceCache.getDeviceRoom().toString());
+                if (deviceInfo.has("DHT11")) {
+                    JsonNode data = deviceInfo.get("DHT11");
+                    //温度
+                    if (data.has("Temperature")) {
+                        point.setTemperature(data.get("Temperature").asDouble());
+                    }
+                    //湿度
+                    if (data.has("Humidity")) {
+                        point.setHumidity(data.get("Humidity").asDouble());
+                    }
+                }
+                if (deviceInfo.has("BH1750")) {
+                    JsonNode light = deviceInfo.get("BH1750");
+                    //亮度
+                    if (light.has("Illuminance")) {
+                        point.setIlluminance(light.get("Illuminance").asDouble());
+                    }
+                }
+                //人
+                if (deviceInfo.has("LD2402")) {
+                    JsonNode person = deviceInfo.get("LD2402");
+                    //亮度
+                    if (person.has("Distance")) {
+                        double distance = person.get("Distance").asDouble();
+                        if (distance > 0) {
+                            point.setMotion(1);
+                        } else {
+                            point.setMotion(0);
+                        }
+                    }
+                }
+                influxDBClient.getWriteApiBlocking().writeMeasurement(
+                        influxProperties.getBucket(),
+                        influxProperties.getOrg(),
+                        WritePrecision.MS,
+                        point
+                );
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
