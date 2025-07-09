c493432ae73cf72ed3ac53eee365bc43bbc9bdd8
way <1556080769@qq.com>
2025-07-09 16:47:25 +0800

    bug：分类下拉无数据

--- youlai-boot-master/src/main/java/com/youlai/boot/category/converter/CategoryConverter.java
+++ youlai-boot-master/src/main/java/com/youlai/boot/category/converter/CategoryConverter.java
@@ -4,6 +4,8 @@ import com.youlai.boot.category.model.entity.Category;
 import com.youlai.boot.category.model.form.CategoryForm;
 import com.youlai.boot.common.model.Option;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
@@ -19,6 +21,12 @@ public interface CategoryConverter{
     CategoryForm toForm(Category entity);
 
     Category toEntity(CategoryForm formData);
+    @Mappings({
+            @Mapping(target = "value", source = "id"),
+            @Mapping(target = "label", source = "categoryName")
+    })
+    Option<Long> toOption(Category entity);
 
     List<Option<Long>> toOptions(List<Category> list);
+
 }
