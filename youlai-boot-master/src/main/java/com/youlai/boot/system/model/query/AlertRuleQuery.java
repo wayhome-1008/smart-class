70acab43923ad45b8844ab25336a117d645530de
way <1556080769@qq.com>
2025-07-23 11:37:16 +0800

    分页bug

--- youlai-boot-master/src/main/java/com/youlai/boot/system/model/query/AlertRuleQuery.java
+++ youlai-boot-master/src/main/java/com/youlai/boot/system/model/query/AlertRuleQuery.java
@@ -22,7 +22,5 @@ public class AlertRuleQuery extends BasePageQuery {
     private Long deviceId;
     @Schema(description = "报警级别（继承规则的 level）")
     private Integer level;
-    @Schema(description = "事件状态（0 - 未处理，1 - 已处理，2 - 已忽略）")
-    private String status;
 
 }
