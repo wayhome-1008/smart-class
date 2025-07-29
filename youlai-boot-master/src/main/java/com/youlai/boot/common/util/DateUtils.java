33904a3a6d76cfef71ad57fbb41f7bbdba2b7e37
way <1556080769@qq.com>
2025-07-29 17:46:09 +0800

    整合liteflow

--- youlai-boot-master/src/main/java/com/youlai/boot/common/util/DateUtils.java
+++ youlai-boot-master/src/main/java/com/youlai/boot/common/util/DateUtils.java
@@ -1,81 +1,233 @@
-
 package com.youlai.boot.common.util;
 
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.ReflectUtil;
-import cn.hutool.core.util.StrUtil;
-import com.youlai.boot.device.model.influx.InfluxMqttPlug;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.apache.commons.lang3.time.DateFormatUtils;
 
-import java.lang.reflect.Field;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.lang.management.ManagementFactory;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Date;
+import java.util.Random;
 
 /**
- * 日期工具类
+ * 时间工具类
  *
- * @author haoxr
- * @since 2.4.2
+ * @author ruoyi
  */
-public class DateUtils {
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils
+{
+    public static String YYYY = "yyyy";
+
+    public static String YYYY_MM = "yyyy-MM";
+
+    public static String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+
+    public static String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    public static String SS_MM_HH_DD_HH_YY = "ssmmHHddMMyy";
+
+    public static String YY_MM_DD_HH_MM_SS = "yy-MM-dd HH:mm:ss";
+
+    private static String[] parsePatterns = {
+            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
+            "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
+            "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
 
     /**
-     * 区间日期格式化为数据库日期格式
-     * <p>
-     * eg：2021-01-01 → 2021-01-01 00:00:00
+     * 获取当前Date型日期
      *
-     * @param obj                要处理的对象
-     * @param startTimeFieldName 起始时间字段名
-     * @param endTimeFieldName   结束时间字段名
+     * @return Date() 当前日期
      */
-    public static void toDatabaseFormat(Object obj, String startTimeFieldName, String endTimeFieldName) {
-        Field startTimeField = ReflectUtil.getField(obj.getClass(), startTimeFieldName);
-        Field endTimeField = ReflectUtil.getField(obj.getClass(), endTimeFieldName);
+    public static Date getNowDate()
+    {
+        return new Date();
+    }
 
-        if (startTimeField != null) {
-            processDateTimeField(obj, startTimeField, startTimeFieldName, "yyyy-MM-dd 00:00:00");
+    /**
+     * 获取当前日期, 默认格式为yyyy-MM-dd
+     *
+     * @return String
+     */
+    public static String getDate()
+    {
+        return dateTimeNow(YYYY_MM_DD);
+    }
+
+    public static final String getTime()
+    {
+        return dateTimeNow(YYYY_MM_DD_HH_MM_SS);
+    }
+
+    public static final String dateTimeNow()
+    {
+        return dateTimeNow(YYYYMMDDHHMMSS);
+    }
+
+    public static final String dateTimeNow(final String format)
+    {
+        return parseDateToStr(format, new Date());
+    }
+
+    public static final String dateTime(final Date date)
+    {
+        return parseDateToStr(YYYY_MM_DD, date);
+    }
+
+    public static final String parseDateToStr(final String format, final Date date)
+    {
+        return new SimpleDateFormat(format).format(date);
+    }
+
+    public static final Date dateTime(final String format, final String ts)
+    {
+        try
+        {
+            return new SimpleDateFormat(format).parse(ts);
         }
-
-        if (endTimeField != null) {
-            processDateTimeField(obj, endTimeField, endTimeFieldName, "yyyy-MM-dd 23:59:59");
+        catch (ParseException e)
+        {
+            throw new RuntimeException(e);
         }
     }
 
     /**
-     * 处理日期字段
-     *
-     * @param obj           要处理的对象
-     * @param field         字段
-     * @param fieldName     字段名
-     * @param targetPattern 目标数据库日期格式
+     * 日期路径 即年/月/日 如2018/08/08
      */
-    private static void processDateTimeField(Object obj, Field field, String fieldName, String targetPattern) {
-        Object fieldValue = ReflectUtil.getFieldValue(obj, fieldName);
-        if (fieldValue != null) {
-            // 得到原始的日期格式
-            String pattern = field.isAnnotationPresent(DateTimeFormat.class) ? field.getAnnotation(DateTimeFormat.class).pattern() : "yyyy-MM-dd";
-            // 转换为日期对象
-            DateTime dateTime = DateUtil.parse(StrUtil.toString(fieldValue), pattern);
-            // 转换为目标数据库日期格式
-            ReflectUtil.setFieldValue(obj, fieldName, dateTime.toString(targetPattern));
+    public static final String datePath()
+    {
+        Date now = new Date();
+        return DateFormatUtils.format(now, "yyyy/MM/dd");
+    }
+
+    /**
+     * 日期路径 即年/月/日 如20180808
+     */
+    public static final String dateTime()
+    {
+        Date now = new Date();
+        return DateFormatUtils.format(now, "yyyyMMdd");
+    }
+
+
+    /**
+     * 日期路径 即年/月/日 如20180808
+     */
+    public static final String dateTimeYY(Date date)
+    {
+        return DateFormatUtils.format(date, YY_MM_DD_HH_MM_SS);
+    }
+
+    /**
+     * 日期型字符串转化为日期 格式
+     */
+    public static Date parseDate(Object str)
+    {
+        if (str == null)
+        {
+            return null;
+        }
+        try
+        {
+            return parseDate(str.toString(), parsePatterns);
+        }
+        catch (ParseException e)
+        {
+            return null;
         }
     }
 
-    public static Optional<InfluxMqttPlug> findEarliest(List<InfluxMqttPlug> objects) {
-        if (objects == null || objects.isEmpty()) {
-            return Optional.empty();
-        }
-        return objects.stream()
-                .min(Comparator.comparing(InfluxMqttPlug::getTime));
+    /**
+     * 获取服务器启动时间
+     */
+    public static Date getServerStartDate()
+    {
+        long time = ManagementFactory.getRuntimeMXBean().getStartTime();
+        return new Date(time);
     }
 
-    public static Optional<InfluxMqttPlug> findLatest(List<InfluxMqttPlug> objects) {
-        if (objects == null || objects.isEmpty()) {
-            return Optional.empty();
+    /**
+     * 计算相差天数
+     */
+    public static int differentDaysByMillisecond(Date date1, Date date2)
+    {
+        return Math.abs((int) ((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)));
+    }
+
+    /**
+     * 计算时间差
+     *
+     * @param endDate 最后时间
+     * @param startTime 开始时间
+     * @return 时间差（天/小时/分钟）
+     */
+    public static String timeDistance(Date endDate, Date startTime)
+    {
+        long nd = 1000 * 24 * 60 * 60;
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+        // long ns = 1000;
+        // 获得两个时间的毫秒时间差异
+        long diff = endDate.getTime() - startTime.getTime();
+        // 计算差多少天
+        long day = diff / nd;
+        // 计算差多少小时
+        long hour = diff % nd / nh;
+        // 计算差多少分钟
+        long min = diff % nd % nh / nm;
+        // 计算差多少秒//输出结果
+        // long sec = diff % nd % nh % nm / ns;
+        return day + "天" + hour + "小时" + min + "分钟";
+    }
+
+    /**
+     * 增加 LocalDateTime ==> Date
+     */
+    public static Date toDate(LocalDateTime temporalAccessor)
+    {
+        ZonedDateTime zdt = temporalAccessor.atZone(ZoneId.systemDefault());
+        return Date.from(zdt.toInstant());
+    }
+
+    /**
+     * 增加 LocalDate ==> Date
+     */
+    public static Date toDate(LocalDate temporalAccessor)
+    {
+        LocalDateTime localDateTime = LocalDateTime.of(temporalAccessor, LocalTime.of(0, 0, 0));
+        ZonedDateTime zdt = localDateTime.atZone(ZoneId.systemDefault());
+        return Date.from(zdt.toInstant());
+    }
+
+    public static long getTimestamp(){
+        return System.currentTimeMillis();
+    }
+
+    public static long getTimestampSeconds(){
+        return System.currentTimeMillis()/1000;
+    }
+
+    public static String generateRandomHex(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        // 添加"D"作为开头
+        sb.append("D");
+        for (int i = 1; i < length; i++) {
+            int randomInt = random.nextInt(16); // 生成0到15的随机整数
+            char hexChar = Character.toUpperCase(Character.forDigit(randomInt, 16)); // 将整数转换为十六进制字符并转为大写
+            sb.append(hexChar);
         }
-        return objects.stream()
-                .max(Comparator.comparing(InfluxMqttPlug::getTime));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Date date = DateUtils.dateTime(SS_MM_HH_DD_HH_YY, "434123181121");
+        String s = DateUtils.dateTimeYY(date);
+        System.out.println(s);
+
+        String s1 = generateRandomHex(12);
+        System.out.println(s1);
+
     }
 }
